0bc0a36786482c06b32998505cc71a76e31635bb
htaustin <socool.king@gmail.com>
2017-07-27 23:06:02 -0400

    upd indent

--- extractAnswer.py
+++ extractAnswer.py
@@ -26,55 +26,55 @@ xml='''<top>
 
 
 def load_data(fname):
-  lines = open(fname).readlines()
-  qids, questions, answers, labels = [], [], [], []
-  num_skipped = 0
-  prev = ''
-  qid2num_answers = {}
-  for i, line in enumerate(lines):
-    line = line.strip()
+    lines = open(fname).readlines()
+    qids, questions, answers, labels = [], [], [], []
+    num_skipped = 0
+    prev = ''
+    qid2num_answers = {}
+    for i, line in enumerate(lines):
+        line = line.strip()
 
-    qid_match = re.match('<QApairs id=\'(.*)\'>', line)
+        qid_match = re.match('<QApairs id=\'(.*)\'>', line)
 
-    if qid_match:
-      qid = qid_match.group(1)
-      qid2num_answers[qid] = 0
+        if qid_match:
+            qid = qid_match.group(1)
+            qid2num_answers[qid] = 0
 
-    if prev and prev.startswith('<question>'):
-      question = line.lower().split('\t')
+        if prev and prev.startswith('<question>'):
+            question = line.lower().split('\t')
 
-    label = re.match('^<(positive|negative)>', prev)
-    if label:
-      label = label.group(1)
-      label = 1 if label == 'positive' else 0
-      answer = line.lower().split('\t')
-      # if len(answer) > 60:
-      #   num_skipped += 1
-      #   continue
-      labels.append(label)
-      answers.append(answer)
-      questions.append(question)
-      qids.append(qid)
-      qid2num_answers[qid] += 1
-    prev = line
-  # print sorted(qid2num_answers.items(), key=lambda x: float(x[0]))
-  print 'num_skipped', num_skipped
-  return qids, questions, answers, labels
+        label = re.match('^<(positive|negative)>', prev)
+        if label:
+            label = label.group(1)
+            label = 1 if label == 'positive' else 0
+            answer = line.lower().split('\t')
+            # if len(answer) > 60:
+            #   num_skipped += 1
+            #   continue
+            labels.append(label)
+            answers.append(answer)
+            questions.append(question)
+            qids.append(qid)
+            qid2num_answers[qid] += 1
+        prev = line
+    # print sorted(qid2num_answers.items(), key=lambda x: float(x[0]))
+    print 'num_skipped', num_skipped
+    return qids, questions, answers, labels
 
 
 
 
 if __name__ == '__main__':
 
-  stoplist = None
+    stoplist = None
 
 
-  train = 'data/train.xml'
-  train_all = 'data/train-all.xml'
-  # train_files = [train, train_all]
-  train_files = [train_all]
-  for train in train_files:
-    print train
+    train = 'data/train.xml'
+    train_all = 'data/train-all.xml'
+    # train_files = [train, train_all]
+    train_files = [train_all]
+    for train in train_files:
+        print train
 
     train_basename = os.path.basename(train)
     name, ext = os.path.splitext(train_basename)
@@ -91,7 +91,7 @@ if __name__ == '__main__':
     qids, questions, answers, labels = load_data(all_fname)
 
 
-    
+
     writeAnsF = open("allQAAnswers.topics.xml",'w')
     writeAnsListF = open("allQAAnswers.topics.list",'w')
 
@@ -101,14 +101,14 @@ if __name__ == '__main__':
     count = 0
     preQid = 0
     for qid, ans, que, label in zip( qids, answers, questions, labels):
-      count += 1
-      ansXml = xml%(count,' '.join([str(x) for x in ans]))
-      writeAnsF.write(ansXml)
-      writeAnsListF.write(qid+ '\t' +str(count)+ '\t'+str(label)  +'\t'+ ' '.join([str(x) for x in ans])+'\n')
+        count += 1
+        ansXml = xml%(count,' '.join([str(x) for x in ans]))
+        writeAnsF.write(ansXml)
+        writeAnsListF.write(qid+ '\t' +str(count)+ '\t'+str(label)  +'\t'+ ' '.join([str(x) for x in ans])+'\n')
 
-      queXml = xml%(count,' '.join([str(x) for x in que]))
-      writeQueF.write(queXml)
-      writeQueListF.write(qid+ '\t' +str(count)+ '\t'+str(label)  +'\t'+ ' '.join([str(x) for x in que])+'\n')
+        queXml = xml%(count,' '.join([str(x) for x in que]))
+        writeQueF.write(queXml)
+        writeQueListF.write(qid+ '\t' +str(count)+ '\t'+str(label)  +'\t'+ ' '.join([str(x) for x in que])+'\n')
 
     writeAnsF.close()
     writeAnsListF.close()
